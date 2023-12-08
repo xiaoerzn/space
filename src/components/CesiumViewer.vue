c4f01e59a10e38acf73cc7bacf11c2e03a660b67
xiaoerzn <xiaoerzn@163.com>
2023-12-08 17:28:40 +0800

    降低cesium版本，开启实体学习

--- src/components/CesiumViewer.vue
+++ src/components/CesiumViewer.vue
@@ -5,15 +5,17 @@
 <script setup>
 import * as Cesium from "cesium";
 import { onMounted } from "vue";
+let viewer,imageryLayers;
 
 onMounted(async()=>{
     initCesium();
+    addImageryProvider();
 }
 
 );
 
 function initCesium(){
-    const viewer = new Cesium.Viewer("cesiumContainer", {
+    viewer = new Cesium.Viewer("cesiumContainer", {
         animation: true, //动画小部件
         baseLayerPicker: true, //地图图层组件
         fullscreenButton: true, //全屏组件
@@ -28,14 +30,18 @@ function initCesium(){
     });
     // 隐藏版权信息
     viewer._cesiumWidget._creditContainer.style.display = "none";
+    // -----------------------地形数据
+    // const terrainProvider = new Cesium.CesiumTerrainProvider({
+    //     url:Cesium.IonResource.fromAssetId(3956),
+    //     requestWaterMask:true,//请求水体效果所需要的海岸线数据
+    //     requestVertexNormals:true,//请求地形照明数据
+    // })
+    // viewer.terrainProvider=terrainProvider
+}
+function addImageryProvider(){
     // 去除默认layers
     // imageryLayers.remove(imageryLayers.get(0))
-
-    const imageryLayers = viewer.imageryLayers
-// -----------------------------添加图层法一，利用addImageryProvider   
-    // imageryLayers.addImageryProvider(gaoDeImageryProvider)
-
-    //  -----------------------------添加图层法二，利用add
+    imageryLayers = viewer.imageryLayers
     var gaoDeImageryProvider = new Cesium.UrlTemplateImageryProvider({
         url: "http://webst02.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scale=1&style=8",
         minimumLevel: 3,
@@ -43,16 +49,11 @@ function initCesium(){
     })
     var imageryLayer = new Cesium.ImageryLayer(gaoDeImageryProvider);
     imageryLayers.add(imageryLayer)
-
-    // -----------------------地形数据
-    const terrainProvider = new Cesium.CesiumTerrainProvider.fromUrl({
-        url:Cesium.IonResource.fromAssetId(3956),
-        requestWaterMask:true,//请求水体效果所需要的海岸线数据
-        requestVertexNormals:true,//请求地形照明数据
-    })
-    viewer.terrainProvider=terrainProvider
-
-    // --------------------------------CZML数据
+    // -----------------------------添加图层法一，利用addImageryProvider   
+    // imageryLayers.addImageryProvider(gaoDeImageryProvider)
+}
+function addEntities(){
+    viewer.entities.removeAll() //加载之前先清楚所有entity
 }
 </script>
 
